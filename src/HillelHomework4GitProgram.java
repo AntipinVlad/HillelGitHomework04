acb52a0652f813ab58f8cc13efc5f1627777bed0
AntipinVlad <antipinmail1@gmail.com>
2024-01-12 19:44:30 +0200

    second commit to git

--- src/HillelHomework4GitProgram.java
+++ src/HillelHomework4GitProgram.java
@@ -1,5 +1,6 @@
 public class HillelHomework4GitProgram {
     public static void main(String[] args) {
         System.out.println("Hello world from Vlad to Git (Homework)");
+        System.out.println("Second commit to git");
     }
 }
